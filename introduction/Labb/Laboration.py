58433b6227749b1b4b5fa2cb06a01bb803609ed5
Tobias Öberg <tobias.oberg@iths.se>
2025-01-27 13:37:38 +0100

    update

---
+++ introduction/Labb/Laboration.py
@@ -0,0 +1,69 @@
+import numpy as np
+import scipy.stats as stats
+
+# Syy = (n*np.sum(np.square(Y)) - np.square(np.sum(Y)))/n
+# SSR = Syy - SSE
+
+class LinearRegression:
+   
+    def __init__(self, Y, X):
+        self.Y = Y
+        self.X = np.column_stack([np.ones(Y.shape[0]), X])
+        self.n = self.X.shape[0]
+        self.k = self.X.shape[1] - 1
+        self.b = self.fit()
+        self.SSE = np.sum(np.square(self.Y-(self.X @ self.b)))
+        self.Syy = (self.n *np.sum(np.square(self.Y)) - np.square(np.sum(self.Y)))/self.n
+        self.variance = self.calc_variance()
+        self.SSR = self.Syy - self.SSE
+        self.S = np.sqrt(self.variance)
+        
+  
+    
+    def fit(self):
+        self.b = np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.Y
+        return self.b
+
+
+    def calc_variance(self):
+
+        variance = np.var(self.Y)
+        return variance
+        
+
+
+    def calc_std(self):
+       standard_deviation = np.std(self.Y)
+       return standard_deviation
+
+
+    def significance_regression(self):
+        n = len(self.X)
+        k = len(self.b)-1
+        
+        
+        
+
+        sig_statistic = (self.SSR/self.k)/self.S
+        p_significance = stats.f.sf(sig_statistic, k, n-k-1)
+
+        return p_significance
+        
+        # finding p-value, SSR = Syy - SSE
+
+
+
+    def relevance_regression(self):
+        c = np.linalg.pinv(self.X.T @ self.X)*self.variance
+        b3_statistic = self.b[3] / (self.S*np.sqrt(c[3,3]))
+        p_b3 = 2*min(stats.t.cdf(b3_statistic, self.n-self.k-1), stats.t.sf(b3_statistic, self.n-self.k-1))
+        
+        Rsq = self.SSR / self.Syy
+
+        return Rsq, p_b3
+    
+ 
+
+
+# if __name__ == "__main__":
+#     main()
