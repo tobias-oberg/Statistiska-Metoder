bd055fdbaa247c5994119ea6953b90e36e20bd10
Tobias Öberg <tobias.oberg@iths.se>
2025-02-05 15:00:42 +0100

    Updated and code fixes

--- introduction/Labb/Laboration.py
+++ introduction/Labb/Laboration.py
@@ -4,20 +4,23 @@ import scipy.stats as stats
 
 class LinearRegression:
    
-    def __init__(self, Y, X, column_names, confidence_level = 0.95):
-        self.column_names = ["Intercept"] + column_names
+    def __init__(self, Y, X):
         self.Y = Y
+        self.X_name = X
         self.X = np.column_stack([np.ones(Y.shape[0]), X])
         self.b = self.fit()
         self.SSE = np.sum(np.square(self.Y-(self.X @ self.b)))
         self.Syy = (self.n *np.sum(np.square(self.Y)) - np.square(np.sum(self.Y)))/self.n
         self.variance = self.calc_variance()
+        self.std = self.calc_std()
         self.SSR = self.Syy - self.SSE
         self.S = np.sqrt(self.variance)
-        self.confidence_level = confidence_level
         
-        
+    @property
+    def column_names(self):
+        return list(self.X_name.columns)
 
+    
     @property 
     def d(self):
         return self.X.shape[1] - 1
@@ -47,7 +50,7 @@ class LinearRegression:
 
 
     def significance_regression(self):
-        sig_statistic = (self.SSR/self.d)/self.S
+        sig_statistic = (self.SSR/self.d)/self.variance
         p_significance = stats.f.sf(sig_statistic, self.d, self.n-self.d-1)
 
         return p_significance
@@ -80,6 +83,7 @@ class LinearRegression:
         pearson_list = []
         removing_intercept = self.X[:,1:]
         
+        
         for i in range(self.d):
             for j in range(i +1, self.d):
                 pearson, p_value = stats.pearsonr(removing_intercept[:, i], removing_intercept[:, j])
@@ -91,26 +95,50 @@ class LinearRegression:
 
 
     def confidence_intervals(self): 
-        XTX_inv = np.linalg.pinv(self.X.T @ self.X)  # Inverse of X'X
-        se_b = np.sqrt(np.diagonal(XTX_inv) * self.variance) 
-    
-        alpha = 1 - self.confidence_level
-        t_critical = stats.t.ppf(1 - alpha / 2, self.n - self.d - 1)
+        XTX_inv = np.linalg.pinv(self.X.T @ self.X) *self.variance  # Inverse of X'X
+        alpha = 1 - self.confidence_level 
+        t_critical = stats.t.ppf(alpha / 2, self.n - self.d - 1)
+        
 
         ci = []
-        for i in range(len(self.b)):
-            low = self.b[i] - t_critical * se_b[i]  
-            high = self.b[i] + t_critical * se_b[i]  
-            ci.append((self.column_names[i], low, high)) 
+        for i in range(1,len(self.b)):
+            low = self.b[i] - t_critical *self.std * np.sqrt(XTX_inv[i][i])
+            high = self.b[i] + t_critical *self.std * np.sqrt(XTX_inv[i][i])  
+            ci.append((self.column_names[i-1], low, high)) 
 
         return ci
 
-    # scipy ppf Z 'alpha'/2
     
     @property
     def confidence_level(self):
-        return self._confidence_level
+        R2 = self.relevance_regression()
+        if R2 >= 0.997:
+            confidence_level = 0.997
+            return confidence_level
+        elif R2 >= 0.95:
+            confidence_level = 0.95
+            return confidence_level
+        elif R2 >= 0.68:
+            confidence_level = 0.68
+            return confidence_level
+        else: 
+            print("The relevance of your model is too low!")
+            
+
+
  
-    @confidence_level.setter
-    def confidence_level(self, value):
-        self._confidence_level = value
+    
+
+    def observer_bias(self, feature):
+        self.column_names.index(feature)
+        j = self.column_names.index(feature) + 1
+        
+        c = np.linalg.pinv(self.X.T @ self.X)*self.variance
+        b3_statistic = self.b[j] / (self.S*np.sqrt(c[j,j]))
+        p_b3 = 2*min(stats.t.cdf(b3_statistic, self.n-self.d-1), stats.t.sf(b3_statistic, self.n-self.d-1))
+        
+            
+        return feature, p_b3 
+
+
+
